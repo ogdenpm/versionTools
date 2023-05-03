2e11528290b596f0f405d45c6c5e6b001a0b47ec
Mark Ogden <mark.pm.ogden@btinternet.com>
2023-05-03 15:30:57 +0100

    added c++ support to showVersion and added version stamp

--- showVersion.h
+++ showVersion.h
@@ -1,21 +1,38 @@
-#ifdef CPM
+/* showVersion.h: _REVISION_ */
+#ifndef _SHOWVERSION_H_
+#define _SHOWVERSION_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef CPM  /* don't add overhead to CP/M builds */
 #define CHK_SHOW_VERSION(argc, argv)
+
 #else
-#include <stdio.h>
+
 #include <stdbool.h>
+#include <stdio.h>
 
 typedef char const verstr[];
 #ifdef _MSC_VER
 #ifndef strcasecmp
-#define strcasecmp _stricmp
-#define strncasecmp _strnicmp
+#define strcasecmp  _stricmp
 #endif
 #endif
 
 void showVersion(bool full);
-#define CHK_SHOW_VERSION(argc, argv) \
-    if (argc == 2 && strcasecmp(argv[1], "-v") == 0) { \
-        showVersion(argv[1][1] == 'V'); \
-        exit(0); \
-    }
+
+#define CHK_SHOW_VERSION(argc, argv)                  \
+    if (argc == 2 && strcasecmp(argv[1], "-v") == 0)  \
+        do {                                          \
+            showVersion(argv[1][1] == 'V');           \
+            exit(0);                                  \
+    } while (0)
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
 #endif
